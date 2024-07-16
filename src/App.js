2aee00c2f219c87e581fe57a8b3544e998950079
Dakalo988 <dakalosadiki98@gmail.com>
2024-07-16 18:24:32 +0200

    Changes to be committed: 	modified:   src/App.js 	modified:   src/components/Layout/Header.css 	modified:   src/components/Layout/Header.js 	new file:   src/components/Layout/Login.css 	new file:   src/components/Layout/Login.js 	modified:   src/components/ParentComponent.js.js

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LocalPage from './components/LocalPage.js';
 import ParentComponent from './components/ParentComponent.js';
 import PropertyList from './Pages/PropertyList.js';
-import Listing from './Listing/Listing.js'; // Make sure to import Listing component
+import Listing from './Listing/Listing.js'; 
+import Login from "./components/Layout/Login.js";
 
 const App = () => {
   return (
     <div className="app">
       <Router>
         <Routes>
-          <Route path="/" element={<ParentComponent />}/>
+          <Route path="/ParentComponent" element={<ParentComponent />}/>
           <Route path="/local-page" element={<LocalPage/>}/>
           <Route path="/PropertyList" element={<PropertyList/>}/>
           <Route path="/listing/:name" element={<Listing/>}/> 
+          <Route path="/Login" element={<Login/>} />
         </Routes>
       </Router>
     </div>
--- src/components/ParentComponent.js.js
+++ src/components/ParentComponent.js.js
@@ -1,35 +1,35 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import Search from './Layout/Search';
-import Header from './Layout/Header';
-import Home from './Home';
-import Inspiration from './Inspiration';
-import Footer from './Footer';
-import Banner from './Banner';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import Search from "./Layout/Search";
+import Header from "./Layout/Header";
+import Home from "./Home";
+import Inspiration from "./Inspiration";
+import Footer from "./Footer";
+import Banner from "./Banner";
 
 const ParentComponent = () => {
   const navigate = useNavigate();
 
   const handleLocationSelect = (location) => {
-    if (location === 'local') {
-      navigate('/local-page'); // Navigate to the desired route
+    if (location === "local") {
+      navigate("/local-page"); // Navigate to the desired route
     }
   };
 
-  console.log('ParentComponent rendering, handleLocationSelect:', handleLocationSelect);
+  console.log(
+    "ParentComponent rendering, handleLocationSelect:",
+    handleLocationSelect
+  );
 
   return (
-
     <div>
-      <Home/>
-      <Header/>
+      <Home />
+      <Header />
       <Search onLocationSelect={handleLocationSelect} />
-      <Banner/>
-      <Inspiration/>
-      <Footer/>
-     
+      <Banner />
+      <Inspiration />
+      <Footer />
     </div>
-    
   );
 };
 
